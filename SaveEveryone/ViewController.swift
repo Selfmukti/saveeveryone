9543dcdd5c1f0eca3a30caf6c492ab950e1e5591
Виктор Опанасенко <viktor@macbook-pro-viktor.local>
2016-09-15 20:39:27 +0300

    Initial Commit

---
+++ SaveEveryone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveEveryone
+//
+//  Created by Виктор Опанасенко on 15.09.16.
+//  Copyright © 2016 Victorious. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
